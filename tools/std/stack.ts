11ef97aa6a17034532d1fedd678a39cc36932331
peaceshi <peaceshi@outlook.com>
2019-08-29 19:43:44 +0800

    add base stack

---
+++ tools/std/stack.ts
@@ -0,0 +1,43 @@
+/**
+ * std.Stack()
+ * 
+ * base Stack implement used Array.
+ *
+ * design by https://en.cppreference.com/w/cpp/container/stack
+ */
+export class Stack<T>{
+	protected element: T[] = null;
+	public constructor(elements?:any,...rest:any[]) {
+		this.element = new Array<T>().concat(elements,...rest);
+	}
+	/**
+	 * checks whether the underlying container is empty
+	 */
+	public empty = () =>this.element.length == 0;
+	/**
+	 * returns the number of element
+	 */
+	public size = () => this.element.length;
+	/**
+	 * inserts element at the end
+	 */
+	public push = (element: any) => this.element.push(element);
+	/**
+	 * removes the first element
+	 * 
+	 * make sure it is FILO
+	 */
+	public pop = () => this.element.pop();
+	/**
+	 * constructs element in-place at the end
+	 */
+	public emplace = (Stack: Stack<T>, ...elements: any[]) => elements.forEach((element) => Stack.push(element));
+	/**
+	 * delete all elements
+	 */
+	public clean = () => { this.element = [] };
+	/**
+	 * swaps the contents
+	 */
+	//public swap = (source: Stack<T>, dist: Stack<T>) => {};
+}
