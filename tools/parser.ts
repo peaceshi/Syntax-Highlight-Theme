48e3f8b2613e3cb620be9ba64ee03c2ea01bc6c5
peaceshi <peaceshi@outlook.com>
2019-09-23 17:19:14 +0800

    code refactoring

--- tools/parser.ts
+++ tools/parser.ts
@@ -2,70 +2,104 @@
  * https://github.com/microsoft/node-jsonc-parser
  */
 import jsonc = require("jsonc-parser");
-import fs = require("fs");
-import path = require("path");
 import std = require("./std");
-/**
- * "../tools"
- */
-const rootPath = path.normalize(__dirname.concat("/.."));
-const file = fs.readFileSync(rootPath.concat("/themes/syntax-material-dark-color-theme.json"), 'utf-8');
-
-let jsonTree = jsonc.parseTree(file);
-
-let deque = new std.Deque<any>();
 
 /**
- *  set onlyScope only output Scope
+ * parse
+ * @param file the json file
+ * @param type 0 is theme file, 1 is tmLanguage file.
  */
-let onlyScope: boolean = false;
-/**
- * 
- * @param node jsonNode
- * @param insertPosition make sure the json document structure output correctly.
- */
-let nodeTraversal = (node: jsonc.Node, insertPosition: boolean) => {
-	if (!(typeof (node) == "undefined")) {
-		if (node.type == "string") {
-			if (onlyScope) {
-				console.log(node.value);
-			}
-		}
-		else {
-			node.children.forEach(node => {
-				if (node.type == "string") {
-					if (!onlyScope) {
-					//	console.log(node.value);
-					}
-				}
-				if (node.value == "scope") {
-					onlyScope = true;
-				}
-				if (node.value == "settings") {
-					onlyScope = false;
-				}
-				if (node.type == "object") {
-					deque.push_front(node);
-					nodeTraversal(deque.pop_front(), true);
-				}
-				else if (node.type == "array") {
-					node.children.reverse().forEach(node => {
-						deque.push_front(node);
-					});
-				}
-				else {
-					if (insertPosition) {
-						deque.push_front(node);
-					} else {
-						deque.push_back(node);
-					}
-				}
-			}
-			);
+export
+let parser = (file: string, type: number) => {
+	let deque = new std.Deque<any>();
+	let array = new Array<string>();
+	let set = new Set<string>();
+
+	let onlyScope: boolean = false;
+	let is_tmLanguage: boolean = false;
+	let tmFlag: boolean = false;
+
+	let jsonTree = jsonc.parseTree(file);
+
+	switch (type) {
+		case 0:
+			onlyScope = false; //must be false
+			break;
+		case 1:
+			is_tmLanguage = true;
+			break;
+	}
+	/**
+	 * use ES6 "Set()" to remove duplicates
+	 */
+	let addSet = (value: string) => {
+		if (value.indexOf(" ")) {
+			value.split(" ").forEach(key => {
+				set.add(key);
+			});
+		} else {
+			set.add(value);
 		}
 	}
-	while (!deque.empty()) {
-		nodeTraversal(deque.pop_front(), false);
+	/**
+	* nodeTraversal
+	* @param node jsonNode
+	* @param insertPosition make sure the json document structure output correctly.
+	* 
+	* insertPosition = true : push_front;
+	* 
+	* insertPosition = false : push_back;
+	*/
+	let nodeTraversal = (node: jsonc.Node, insertPosition: boolean) => {
+		if (!(typeof (node) == "undefined")) {
+			if (node.type == "string") {
+				if (onlyScope || tmFlag) {
+					addSet(node.value);
+					tmFlag = false;
+				}
+			}
+			else {
+				node.children.forEach(node => {
+					if (node.value == "scope") {
+						onlyScope = true;
+					}
+					if (node.value == "settings") {
+						onlyScope = false;
+					}
+					if (tmFlag) {
+						addSet(node.value);
+						tmFlag = false;
+					}
+					if (is_tmLanguage && node.value == "name") {
+						tmFlag = true;
+					}
+					if (node.type == "object") {
+						deque.push_front(node);
+						nodeTraversal(deque.pop_front(), true);
+					}
+					else if (node.type == "array") {
+						node.children.reverse().forEach(node => {
+							deque.push_front(node);
+						});
+					}
+					else {
+						if (insertPosition) {
+							deque.push_front(node);
+						} else {
+							deque.push_back(node);
+						}
+					}
+				}
+				);
+			}
+		}
+		while (!deque.empty()) {
+			nodeTraversal(deque.pop_front(), false);
+		}
 	}
+	nodeTraversal(jsonTree, false);
+
+	array = Array.from(set).sort();
+
+	return array;
 }
-nodeTraversal(jsonTree, false);
