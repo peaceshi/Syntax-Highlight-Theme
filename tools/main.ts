af24aec7e3b49f9970efa0a6b0cb772981f504c8
peaceshi <peaceshi@outlook.com>
2019-10-27 23:03:23 +0800

    update

--- tools/main.ts
+++ tools/main.ts
@@ -3,6 +3,5 @@ import JSONC = require("./parser");
 
 const tmFilePath = file.mainPath.root.concat("/templates/*.json");
 //const file_0 = fs.readFileSync(mainPath.rootPath.concat("/templates/cpp.tmLanguage.json"), 'utf-8');
-
 file.fetchFiles();
 JSONC.parser(tmFilePath);
