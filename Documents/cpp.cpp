4a181be397fc6744262eb3291d372bc20abac380
PeaceShi <peaceshi@outlook.com>
2019-12-24 01:26:15 +0800

    add cpp test file

---
+++ Documents/cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#define DoubleMax   1.79769e+308
+#define show(argument) cout << #argument << "test,%d\\%s\n";
+#ifndef LIBRARY
+	#define Infinite    numeric_limits<long double>::infinity()
+#endif
+
+using inline namespace scoped::console { 
+	template <typename ANYTYPE> void __MAGIC__show(ANYTYPE input) {
+			// by default use the stream operator with cout
+	[[noreturn]] void func() {
+			using::std::cout << 1'23'456 + 0x0000000C;
+			std::allocator_traits<decltype(copy)>::destroy(copy, this);
+			new (&a_callable.callable) type(forward<Callable>(a));
+		}
+	}
+};
